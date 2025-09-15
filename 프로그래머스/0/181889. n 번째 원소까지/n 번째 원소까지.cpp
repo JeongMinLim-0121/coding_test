20e77eb8447687f081bc917fa98f256af4ce640a
JeongMinLim-0121 <jm02040121@gmail.com>
2025-09-16 08:50:00 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    
+ 
+    return vector<int> (num_list.begin(), num_list.begin()+n);
+}
