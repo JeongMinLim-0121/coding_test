b32b8a21fe1ea0afbbcc59118def4d1f1611e534
JeongMinLim-0121 <jm02040121@gmail.com>
2025-09-18 19:44:05 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    int size = array.size();
+    
+    for(int i=0;i<size; i++){
+        if(array[i]==n){
+            answer++;
+            
+        }
+    }
+    return answer;
+}
