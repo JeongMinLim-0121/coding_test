bc3058869f9910b446429395c30ee9da67b45e7b
JeongMinLim-0121 <jm02040121@gmail.com>
2025-09-19 17:24:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.09 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string,int>cnt;
+    
+    for(const auto& name : participant) ++cnt[name];
+    for(const auto& name : completion) --cnt[name];
+    
+    for(const auto& kv : cnt){
+        if(kv.second > 0) return kv.first;
+    }
+    
+    return "";
+}
