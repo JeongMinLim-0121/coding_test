094bc0e846fec8fd3352f644780e52fe1f2b1821
임정민 <jm02040121@gmail.com>
2026-01-27 11:53:21 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+     
+	int a, b, sum;
+
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum;
+
+    return 0;
+}
