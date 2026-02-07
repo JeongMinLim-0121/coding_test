5fea1c58727d11814fd94ec90738a1638c6dead5
임정민 <jm02040121@gmail.com>
2026-02-07 11:06:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int original = x;
+    int sum=0;
+   
+    while(x>0){
+        sum += (x%10);
+        x /=10;
+    }
+    if(original%sum == 0){
+        answer = true;
+    }else{
+         answer = false;
+    }
+    
+    return answer;
+}
