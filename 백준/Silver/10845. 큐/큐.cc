97b42830f5470f4d5ee85e96396f63a2dbcaf132
임정민 <jm02040121@gmail.com>
2026-02-08 15:41:05 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+ 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    queue<int> q;
+    int num,n;
+    string str;
+    cin >> num;
+
+    for (int i = 0; i < num; i++) {
+        cin >> str;
+        if (str == "push") {
+            cin >> n;
+            q.push(n);
+        }
+        else if (str == "pop") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else { 
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            
+        }
+        else if (str == "size") {
+            cout << q.size() << '\n';
+        }
+        else if (str == "empty") {
+            if (q.empty()) {
+                cout << 1 << '\n';
+            }
+            else {
+                cout << 0 << '\n';
+            }
+        }
+        else if (str == "front") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << q.front() << '\n';
+            }
+        }
+        else if (str == "back") {
+            if (q.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << q.back() << '\n';
+            }
+        }
+    }
+     
+    return 0;
+}
