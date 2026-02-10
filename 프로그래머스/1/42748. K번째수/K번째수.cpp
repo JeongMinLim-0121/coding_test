235ae0c10decc698b1e9f269153b14c56b22857e
임정민 <jm02040121@gmail.com>
2026-02-10 16:24:30 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    int i,j,k;
-    for(auto& a: commands){
-        i=a[0];
-        j=a[1];
-        k=a[2];
-        
-        vector<int> sub(array.begin() +(i- 1), array.begin() + j);
-        
-        sort(sub.begin(), sub.end());
-        answer.push_back(sub[k-1]);
-    }
     
-    
+   for(const vector<int>& cmd:commands ){
+       int i = cmd[0];   
+       int j = cmd[1];   
+       int k = cmd[2];   
+       
+       vector<int>tmp(array.begin()+(i-1),array.begin()+j);
+       sort(tmp.begin(), tmp.end());
+       answer.push_back(tmp[k-1]);
+       
+   }
+     
     return answer;
 }
