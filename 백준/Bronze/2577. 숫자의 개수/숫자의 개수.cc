0d938972488d521c811779239816e768ee3aadcc
임정민 <jm02040121@gmail.com>
2026-02-11 10:45:42 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,33 +1,25 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <vector>
-#include <sstream>
-
+ 
 using namespace std;
-
  
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-     
-    int a, b, c, mul=0;
-    vector<int> cnt(10, 0);
+    vector<int>v(10, 0);
+    int A, B, C;
+    string sum;
+    cin >> A >> B >> C;
 
-    cin >> a >> b >> c;
-
-    mul = a * b * c;
-
-    while (mul > 0) {
-        int digit = mul % 10;
-        cnt[digit]++;
-        mul /= 10;
+    sum = to_string(A*B*C);
+    for (char s : sum) {
+        v[s - '0']++;
     }
     for (int i = 0; i < 10; i++) {
-        cout << cnt[i] << "\n";
-
-    }
+        cout << v[i] << '\n';
+     }
 
     return 0;
 }
-
