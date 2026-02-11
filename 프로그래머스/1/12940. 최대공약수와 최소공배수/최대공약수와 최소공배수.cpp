662fb3cfd2656e677c6d54e6b6cb8dbbc8e3cd43
임정민 <jm02040121@gmail.com>
2026-02-11 12:53:26 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a,int b){
+    while(b !=0){
+        int r=a%b;
+        a=b;
+        b=r;
+    }
+    return a;
+}
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    int g=gcd(n,m);
+    int l=(n/g)*m;
+    
+    return {g,l};
+}
