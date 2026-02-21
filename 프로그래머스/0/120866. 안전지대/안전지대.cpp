5c1b5a395bf8ee2df42591b8ee55fe566d2a2e29
임정민 <jm02040121@gmail.com>
2026-02-21 14:20:55 +0900

    [level 0] Title: 안전지대, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.cpp
+++ 프로그래머스/0/120866. 안전지대/안전지대.cpp
@@ -5,30 +5,34 @@ using namespace std;
 
 int solution(vector<vector<int>> board) {
     int answer = 0;
+    int size = board.size();
     
-    int size=board.size();
-   
-    vector<int> dx = {-1,-1,-1, 0,0, 1,1,1};
-    vector<int> dy = {-1, 0, 1,-1,1,-1,0,1};
+    int dx[8] = {-1,-1,-1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1,-1, 1,-1, 0, 1};
     
     for(int i=0;i<size;i++){
         for(int j=0;j<size;j++){
-            if(board[i][j] == 1 ){
-                   for(int k=0;k<8;k++){
-                       int ni = i+dx[k];
-                       int nj = j+dy[k];
-                       
-                       if(ni>=0 && ni < size && nj>=0 && nj < size){
-                           if(board[ni][nj]==0)board[ni][nj] =2;
-                       }
-                   }
+            if(board[i][j] == 1){
+                for(int k=0;k<8;k++){
+                     int nx = i + dx[k];
+                     int ny = j + dy[k];
+                    
+                    if(nx < size && ny <size  && nx >= 0 && ny >= 0){
+                        if(board[nx][ny] == 0)
+                            board[nx][ny] = 2;
+                    }
+                }
+               
             }
         }
     }
-    for(int i=0;i<size;i++){
-       for(int j=0;j<size;j++){
-           if(board[i][j] ==0) answer++;
-       } 
-    }
+    
+      for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+             if(board[i][j] == 0){
+                 answer++;
+             }
+        }
+      }
     return answer;
 }
