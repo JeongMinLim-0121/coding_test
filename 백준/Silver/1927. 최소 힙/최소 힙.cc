57c836fd2d06d37409092ef9deedcbdf63e61e0f
임정민 <jm02040121@gmail.com>
2026-02-25 15:04:11 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <algorithm>
+#include <queue>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    priority_queue<int, vector<int> , greater<int>> pq;
+    int n,x;
+    cin >> n;
+
+    while (n--) {
+        cin >> x;
+        if (x == 0) {
+            if (pq.empty()) cout << 0 << '\n';
+            else { cout << pq.top() << '\n'; pq.pop(); }
+        }
+        else {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
